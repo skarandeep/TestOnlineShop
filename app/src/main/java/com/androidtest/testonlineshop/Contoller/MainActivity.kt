cca6a745d30a6318ca8cfec5ac6865858ab26a76
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-07-22 10:19:43 -0400

    Step1 - So far so good

--- app/src/main/java/com/androidtest/testonlineshop/Contoller/MainActivity.kt
+++ app/src/main/java/com/androidtest/testonlineshop/Contoller/MainActivity.kt
@@ -2,11 +2,17 @@ package com.androidtest.testonlineshop.Contoller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
 import com.androidtest.testonlineshop.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
